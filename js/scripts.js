0992ed17751d61efa0915500fa6d33bd270bce88
Annie Shin <annieyshin@gmail.com>
2018-06-15 15:38:00 -0700

    add Javascript for size cost and toppings display text

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 var newPizza;
+var pizzaSizeChecked;
 
 function pizza(toppings, size) {
   this.quantity = 0;
@@ -8,38 +9,44 @@ function pizza(toppings, size) {
   this.cost = 0;
 }
 
-pizza.prototype.toppingsPRO = function() {
-  var allTheToppings = this.toppings.length;
-  this.cost += allTheToppings;
-}
-
-// pizza.prototype.costofPizzaSize = function() {
-//   if(this.size === "10") {
-//     this.cost += 10;
-//   } else if (this.size === "15") {
-//     this.cost += 15;
-//   } else if (this.size === "20") {
-//     this.cost += 20;
-//   } else if (this.size === "25") {
-//     this.cost +=25;
-//   }
+// pizza.prototype.toppingsPRO = function() {
+//   var allTheToppings = this.toppings.length;
+//   this.cost += allTheToppings;
 // }
 
-
+pizza.prototype.sizePizzaPRO = function() {
+  if(this.size === "10") {
+    this.cost += 10;
+  } else if (this.size === "15") {
+    this.cost += 15;
+  } else if (this.size === "20") {
+    this.cost += 20;
+  } else if (this.size === "25") {
+    this.cost += 25;
+  }
+}
 
 $(document).ready(function () {
-  $("#orderSubmitBTNID").click(function(event) {
-    event.preventDefault();
-    newPizza = new pizza();
+  newPizza = new pizza();
 
-    $("input:checkbox[name=toppingsCheckBoxes]:checked").each(function() {
-      var toppingsTotalChecked = $(this).val();
-      newPizza.toppings.push(toppingsTotalChecked);
+      $("input[name='sizeCheckBoxes']").click(function(){
+      $("input:radio[name=sizeCheckBoxes]:checked").each(function(){
+        pizzaSizeChecked = $(this).val();
+        newPizza.size = pizzaSizeChecked;
+        console.log(pizzaSizeChecked);
+      });
 
-    $(".pizzaOrderListID").text("Toppings: " + newPizza.toppings + "  ");
-    console.log("brutal");
+      $("#orderSubmitBTNID").click(function(event) {
+        event.preventDefault();
+        $("input:checkbox[name=toppingsCheckBoxes]:checked").each(function() {
+          var toppingsTotalChecked = $(this).val();
+          newPizza.toppings.push(toppingsTotalChecked);
+          console.log("woof");
+        });
 
+        newPizza.sizePizzaPRO();
+        $(".pizzaOrderListID").text("Cost: " + newPizza.cost + "  Toppings: " + newPizza.toppings);
+        console.log("brutal")
+      });
     });
-  });
 });
-// });

28abef9115bea060a28d537e4c9991099ffda26a
Annie Shin <annieyshin@gmail.com>
2018-06-15 12:12:45 -0700

    change the radio buttons to check boxes to allow for user to check more than one topping

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,31 @@
 var pizzaVAR = "";
 var orderTotalVAR = "";
-
+var toppingAmount = function() {
+  for(var i = 0; i<radio.length; i++) {
+    if(radio[i].checked) {
+      return radio[i].value;
+    }
+  }
+ return '';
+};
 function pizza(toppings, size) {
   this.quantity = 0;
-  this.toppings = toppings;
+  this.toppings = [];
   this.size = size;
   this.customerName;
+  this.cost = 0;
 }
 
-pizza.prototype.costPizza = function() {
-  return this.topping = toppingAmount * 1.50;
-
+pizza.prototype.costofPizzaSize = function() {
+  if(this.size === "10") {
+    this.cost += 10;
+  } else if (this.size === "15") {
+    this.cost += 15;
+  } else if (this.size === "20") {
+    this.cost += 20;
+  } else if (this.size === "25") {
+    this.cost +=25;
+  }
 }
 
 
@@ -19,6 +34,11 @@ $(document).ready(function () {
   $("#orderSubmitBTNID").click(function(event) {
     event.preventDefault();
     newPizzaOrder = new pizza()
+    $("#pizzaToppingsFormID input:radio:checked").each(function(){
+      var toppingsOrder = $(this).val();
+      newPizzaOrder.toppings.push(toppingsOrder);
+    });
+
     $("#pizzaOrderListID").text("Cost: " + newPizzaOrder);
   });
 });

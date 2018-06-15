1687673b06049e26cdf3bbb67c7d521f476fbe6e
Annie Shin <annieyshin@gmail.com>
2018-06-15 10:53:01 -0700

    added more Js for pizza object and constructor

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,20 @@ function pizza(toppings, size) {
   this.quantity = 0;
   this.toppings = toppings;
   this.size = size;
-  this.customerName = customerName;
+  this.customerName;
 }
 
+pizza.prototype.costPizza = function() {
+  return this.topping = toppingAmount * 1.50;
+
+}
 
 
 
 $(document).ready(function () {
   $("#orderSubmitBTNID").click(function(event) {
     event.preventDefault();
-    $("#pizzaOrderListID").append("<li>" + "hello!" + "</li>");
+    newPizzaOrder = new pizza()
+    $("#pizzaOrderListID").text("Cost: " + newPizzaOrder);
   });
 });
